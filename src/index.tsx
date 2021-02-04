3cb84b0c25785f97b25b266534ff9f9ac84783bf
batablo <mutemath.us22@gmail.com>
2021-02-04 12:57:04 +0900

    Add: TypeScriptでsetState記述

---
+++ src/index.tsx
@@ -0,0 +1,92 @@
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable max-classes-per-file */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+interface SquarePropsInterface {
+  value: number;
+}
+
+interface SquareStateInterface {
+  value: string;
+}
+
+class Square extends React.Component<
+  SquarePropsInterface,
+  SquareStateInterface
+> {
+  constructor(props: SquarePropsInterface) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+  }
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <button
+        className="square"
+        type="button"
+        onClick={() => {
+          this.setState({ value: 'X' });
+        }}
+      >
+        {value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare = (i: number) => {
+    return <Square value={i} />;
+  };
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById('root'));

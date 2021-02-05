7d99b148b3450c0b54611fc9799e1aba8515b0ba
batablo <mutemath.us22@gmail.com>
2021-02-05 13:24:27 +0900

    Update: stateをGameが保持するように変更

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,95 +1,35 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { Square } from './Square';
 
 export interface BoardPropsInterface {
   squares: string[];
+  onClick: (i: number) => void;
 }
 
-export interface BoardStateInterface {
-  squares: string[];
-  xIsNext: boolean;
-}
+export const Board: FC<BoardPropsInterface> = (props) => {
+  const renderSquare = (i: number): JSX.Element => {
+    const { squares, onClick } = props;
 
-const calculateWinner = (squares: string[]): string => {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-
-  return '';
-};
-
-export class Board extends React.Component<
-  BoardPropsInterface,
-  BoardStateInterface
-> {
-  constructor(props: BoardPropsInterface) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill('') as string[],
-      xIsNext: true,
-    };
-  }
-
-  handleClick = (i: number): void => {
-    const { squares, xIsNext } = this.state;
-    const sliceSquares: string[] = squares.slice();
-    // 既に決着が着いているか、マスが埋まっていたらreturn
-    if (calculateWinner(sliceSquares) || sliceSquares[i]) {
-      return;
-    }
-    sliceSquares[i] = xIsNext ? 'X' : 'O';
-    this.setState({ squares: sliceSquares, xIsNext: !xIsNext });
+    return <Square value={squares[i]} onClick={() => onClick(i)} />;
   };
 
-  renderSquare = (i: number): JSX.Element => {
-    const { squares } = this.state;
-
-    return <Square value={squares[i]} onClick={() => this.handleClick(i)} />;
-  };
-
-  render(): JSX.Element {
-    const { squares, xIsNext } = this.state;
-    const winner = calculateWinner(squares);
-    let status;
-    if (winner) {
-      status = `Winner: ${winner}`;
-    } else {
-      status = `Next player: ${xIsNext ? 'X' : 'O'}`;
-    }
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
-}
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,95 @@
-/* eslint-disable react/prefer-stateless-function */
-/* eslint-disable max-classes-per-file */
-import React, { FC } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Board } from './components/Board';
 import './index.css';
 
-const Game: FC = () => {
-  return (
-    <div className="game">
-      <div className="game-board">
-        <Board squares={Array(9).fill('')} />
-      </div>
-      <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
-      </div>
-    </div>
-  );
+type Square = 'X' | 'O' | '';
+
+type Squares = {
+  squares: Square[];
 };
+
+interface GameStateInterface {
+  history: Squares[];
+  xIsNext: boolean;
+}
+
+const calculateWinner = (squares: string[]) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+};
+class Game extends React.Component<unknown, GameStateInterface> {
+  constructor(props: unknown) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill('') as Square[],
+        },
+      ],
+      xIsNext: true,
+    };
+  }
+
+  handleClick = (i: number): void => {
+    const { history, xIsNext } = this.state;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{ squares }]),
+      xIsNext: !xIsNext,
+    });
+  };
+
+  render() {
+    const { history, xIsNext } = this.state;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
 
 // ========================================
 

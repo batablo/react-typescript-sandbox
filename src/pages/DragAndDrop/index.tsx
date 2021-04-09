901274711245539e2c8049a40bd2c83650b4f1af
eg-h-obata <obata.eg58@members.co.jp>
2021-04-09 17:15:10 +0900

    add: トップ画面に戻るリンク

--- src/pages/DragAndDrop/index.tsx
+++ src/pages/DragAndDrop/index.tsx
@@ -1,10 +1,14 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { DragAndDrop } from 'components/organisms/DragAndDrop';
 
 export const DragAndDropPage: FC = () => (
   <Wrapper>
     <DragAndDrop />
+    <LinkText>
+      <Link to="/">TopPage</Link>
+    </LinkText>
   </Wrapper>
 );
 
@@ -16,4 +20,11 @@ const Wrapper = styled.div`
   padding: 64px;
   overflow: hidden;
   background-color: lightgray;
+`;
+
+const LinkText = styled.div`
+  position: absolute;
+  bottom: 32px;
+  font-size: 24px;
+  color: blue;
 `;

c4a00117f26181d545bed186f9489a886ec1ca8d
eg-h-obata <obata.eg58@members.co.jp>
2021-04-12 15:08:31 +0900

    delete: 不要なコメントアウト削除

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import { HashRouter } from 'react-router-dom';
 import GlobalStyle from 'scripts/GlobalStyle';
 import { App } from './App';
 
-// const ROUTER_BASENAME =
-//   process.env.NODE_ENV === 'development' ? '/' : '/react-typescript-sandbox';
-
 ReactDOM.render(
   <HashRouter>
     <GlobalStyle />
